cc584cdd04c6a19684bd55d32ddeabffc3d04912
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-13 10:37:46 -0300

    feat: prototipagem de DTO para inserção de estabelecimentos

---
+++ requestDTOs/StoreDTO.py
@@ -0,0 +1,25 @@
+import pydantic,re
+from pydantic import BaseModel,field_validator,Field
+from validate_docbr import CNPJ
+
+
+class StoreDTO(BaseModel):
+    storeName:str
+    cnpj:str
+    mccCode:str
+    location:str
+
+    @field_validator
+    @classmethod
+    def validate_cnpj(cls,v:str)->str:
+        validator = CNPJ()
+
+        if not validator.validate(v):
+            raise ValueError("CNJP inválido")
+        
+        return v
+    
+    @field_validator
+    @classmethod
+    def clean_cnpj(cls,v:str)->str:
+        return re.sub(r'\D', '', v)
