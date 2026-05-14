1774e6ad9d9738033b605d7a86f5da35d11348a6
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-14 11:07:49 -0300

    refactor: alteração do serviço de inserção de loja para o padrão com DTO

--- services/InsertStore.py
+++ services/InsertStore.py
@@ -1,22 +1,17 @@
 from services.DbSetup import DataBaseCon as DBC
 import pymysql
-from validate_docbr import CNPJ
+from schemas.requestDTOs.StoreDTO import StoreDTO
 
 class InsertStore:
     @staticmethod
-    def insertStore(storeName: str,cnpj:str, mccCode:str,location: str):
-        validator = CNPJ()
-
-        if not validator.validate(cnpj):
-            return "Erro: cpnj invalido."
-        
-        cnpjClean = cnpj.replace(".", "").replace("/", "").replace("-", "")
+    def insertStore(t: StoreDTO):
     
         try:
             con = DBC.db_connect()
             with con.cursor() as cur:
                 sql = "INSERT INTO store(store_name,cnpj,mcc_code,location) VALUES(%s,%s,%s,ST_GeomFromText(%s, 4326))"
-                cur.execute(sql,(storeName,cnpjClean,mccCode,location))
+                params = (t.storeName,t.cnpj,t.mccCode,t.location)
+                cur.execute(sql,params)
                 con.commit()
 
         except pymysql.MySQLError as e:
