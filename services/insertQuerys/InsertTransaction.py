9da8570cfe672978df3a68a1ad8f720f3879d561
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-15 09:04:31 -0300

    feat: criação de serviço para inserção de transações

---
+++ services/insertQuerys/InsertTransaction.py
@@ -0,0 +1,24 @@
+from services.DbSetup import DataBaseCon as DBC
+from schemas.requestDTOs.TransactionDTO import TransactionDTO
+import pymysql
+
+class InsertTransaction:
+
+    @staticmethod
+    def insertTransaction(t:TransactionDTO):
+        
+        try:
+            con = DBC.db_connect()
+            with con.cursor() as cur:
+                sql = '''INSERT INTO transactions(transaction_value,transaction_date,
+                transaction_cpf,transaction_location,transaction_status,reason,transaction_store_id_FK
+                VALUES (%s,%s,%s,%s,%s,%s,%s)'''
+                params = (t.value,t.data,t.cpf,t.location,t.status,t.reason,t.storeID)
+                cur.execute(sql,params)
+                con.commit()
+        except pymysql.MySQLError as e:
+            return f"Erro no banco de dados{e}"
+        
+        finally:
+            if 'con' in locals() and con.open:
+                con.close()
