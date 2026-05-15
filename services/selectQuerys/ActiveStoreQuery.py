0b3f67c2b278a2be9a15a6991f7adcf9f63eede3
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-15 10:39:02 -0300

    feat: criação de serviço para verificação do status de atividade de estabelecimentos

---
+++ services/selectQuerys/ActiveStoreQuery.py
@@ -0,0 +1,25 @@
+from services.DbSetup import DataBaseCon as DBC
+import pymysql
+
+
+class ActiveStore:
+
+    @staticmethod
+    def ActiveStore(storeID:id)->bool:
+        try:
+            con = DBC.db_connect()
+            with con.cursor() as cur:
+                sql = 'SELECT is_active FROM store WHERE id = %s'
+                cur.execute(sql,storeID)
+                boolResponse = cur.fetchone()
+
+                if boolResponse:
+                    return True
+                return False
+            
+        except pymysql.MySQLError as e:
+            return f"Erro no banco de dados{e}"
+        
+        finally:
+            if 'con' in locals() and con.open:
+                con.close()
