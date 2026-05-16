497be6bba3aaa88f779c69724252469224bbb00b
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-15 22:47:45 -0300

    fix: inserção do retorno do relatório na função

--- services/selectQuerys/SelectTransactionByDate.py
+++ services/selectQuerys/SelectTransactionByDate.py
@@ -8,6 +8,7 @@ class SelectTransaction():
     @staticmethod
     def selectTransaction(storeID:int,startDate: date,finalDate: date):
         try:
+            
             con = DBC.db_connect()
             with con.cursor() as cur:
                 sql = '''SELECT * FROM transactions WHERE transaction_store_id_FK  = %s and transaction_date BETWEEN %s and %s'''
@@ -15,6 +16,7 @@ class SelectTransaction():
                 response = cur.fetchall()
                 
                 if response:
+
                     relatorio = f"Número de relatórios econtrados na loja{response["transaction_store_id_FK"]: len(response)}\n"    
                     for t in response:
                         relatorio+=(
@@ -27,6 +29,8 @@ class SelectTransaction():
                             f'status: {t['transaction_status']}|\n'
 
                         )
+                        return relatorio
+                    
                 return "Nenhum relatório encontrado."
             
         except pymysql.MySQLError as e:
