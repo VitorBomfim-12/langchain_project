e1c335cde71688b9f86067777bc01737f84f2f0d
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-18 10:47:22 -0300

    feat: criação de query para buscar o total de compras num CPF nas últimas 24 horas

---
+++ services/selectQuerys/ValueByCPF.py
@@ -0,0 +1,32 @@
+from services.DbSetup import DataBaseCon as DBC
+import pymysql
+
+class ValueByCPF:
+
+    @staticmethod
+    def selectValueByCPF(cpf:str):
+        try:
+            con = DBC.db_connect()
+            with con.cursor() as cur:
+                sql = '''
+                SELECT
+                transaction_cpf AS CPF,
+                SUM(transaction_value) AS total_value,
+                COUNT(*) AS total_transactions
+                FROM transactions
+
+                WHERE transaction_date >= NOW() - INTERVAL 1 DAY 
+                AND transaction_cpf = %s
+                GROUP BY transaction_cpf
+                        '''
+                cur.execute(sql,(cpf,))
+                response = cur.fetchall()
+                if response:
+                        data = response[0]
+                        report = f'''Relatório de valor total de compras no CPF :{cpf} nas últimas 24 horas\n
+                        valor total: {data['total_value']}
+                        quantidade de compras: {data['total_transactions']}
+
+                         '''
+                        return report
+                return "nenhuma compra neste CPF nas últimas 24 horas. "
