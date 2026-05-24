f5f10c3250567e35367253ec99e26e3cf59cfb18
VitorBomfim-12 <vbomfimcunha@gmail.com>
2026-05-24 10:55:48 -0300

    feat: criação de função para captação de valor médio das transações de estabelecimentos

---
+++ services/selectQuerys/SelectAVGValue.py
@@ -0,0 +1,29 @@
+from src.first_project.services.DbSetup import DataBaseCon as DBC
+from datetime import datetime
+import pymysql
+
+
+class SelectAVGValue:
+
+    @staticmethod
+    def GetAVGValue(storeID:int,initialDate:datetime,finalDate:datetime)->str:
+        try:
+            con = DBC.db_connect()
+            with con.cursor() as cur:
+                sql = '''SELECT 
+                AVG(transaction_value) as avg_value,
+                FROM transactions
+                WHERE transaction_store_id_FK =v%s AND transaction_date BETWEEN %s AND %s
+                '''
+                cur.execute(sql,storeID,initialDate,finalDate)
+                response = cur.fetchone()
+
+                return response
+            
+        except pymysql.MySQLError as e:
+            print(f"Erro :{e}")
+            return "Erro no banco de dados"
+        
+        finally:
+            if con and con.open:
+                con.close()
